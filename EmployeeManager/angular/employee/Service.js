c5058329d2705c24f097c1d80ddc82efa00398c6
rakib-cse-sust <rakib.cse.sust@gmail.com>
2015-01-19 21:39:07 +0600

    first

---
+++ EmployeeManager/angular/employee/Service.js
@@ -0,0 +1,44 @@
+﻿function employeeService($http, $rootScope, $q) {
+    return {
+        getAllCustomers: GetAllCustomers,
+        getFormStatusType: GetFormStatusType,
+        uploadFileToUrl: UploadFileToUrl
+    };
+
+    function GetFormStatusType() {
+        return {
+            Add: 1,
+            Update: 2
+        }
+    }
+
+    function UploadFileToUrl(file, uploadUrl) {
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        })
+        .success(function () {
+        })
+        .error(function () {
+        });
+    }
+
+    function GetAllCustomers() {
+        var request = $http.get('/Global/Advert/Controllers/RCBSubmitBriefReview/' + activityId + '?briefStatusType=' + briefStatusType);
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function handleError(response) {
+        if (!angular.isObject(response.data) || !response.data.message) {
+            return ($q.reject("An unknown error occurred."));
+        }
+        return ($q.reject(response.data.message));
+    }
+
+    function handleSuccess(response) {
+        return (response.data);
+    }
+
+};
